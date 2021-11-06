ff64397318bec8abc97fb21491b3e5255760dc12
leylakose <leylaboun@gmail.com>
2021-11-05 18:51:39 -0700

    Initial commit

---
+++ password_reminder.py
@@ -0,0 +1,10 @@
+name = 'Leyla'
+password = 'W@12'
+
+entry = input('Enter your name: ')
+
+if entry == name:
+  print(f'Hello {entry}! The password is: {password}')
+else:
+  print(f'Hello {entry}! See you later.')
+
