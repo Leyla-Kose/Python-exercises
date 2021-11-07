0ba4d9d0960d45275ce7d8acee06b3ec34e3cc19
leylakose <leylaboun@gmail.com>
2021-11-07 10:10:18 -0800

    code edited

---
+++ coronavirus.py
@@ -0,0 +1,11 @@
+
+age =  input('Are you a cigarette addict older than 75 years old? Yes or No? ').lower()
+chronic =  input('Do you have a severe chronic disease? Yes or No? ').lower()
+immune = input('Is your immune system too weak? Yes or No? ').lower()
+
+if (age == 'yes') or (chronic == 'yes') or (immune == 'yes'):
+  print('You are in risky group.')
+elif (age == 'no') and (chronic == 'no') and (immune == 'no'):
+  print('You are not in risky group.')
+else: 
+  print('Try again!')
